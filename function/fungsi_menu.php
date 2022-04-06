94899608a5020cf86a7bdc31a50700baeb221838
alibaba04 <albaihaqial@gmail.com>
2022-04-06 11:27:25 +0700

    update dinas luar

--- function/fungsi_menu.php
+++ function/fungsi_menu.php
@@ -8,11 +8,11 @@ require_once('./class/c_user.php');
 function menu() {
     global $dbLink;
     $filter="";
-    if (!empty($_GET['page'])) {
+    if (isset($_GET['page'])) {
         $db_menu = "SELECT * FROM aki_menu WHERE link = '".$_GET['page']."'";
         $d_menu  = mysql_query($db_menu, $dbLink);
         $d_m     = mysql_fetch_assoc($d_menu);
-        $page    = substr(isset($d_m['link'],5));
+        $page    = substr($d_m['link'],5);
         $k_page  = substr($d_m['kodeMenu'],0,2);
     } else {
         $page   = '';
